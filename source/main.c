ec272818c6123c44074240066952e4fd2e7d7724
Okubyona <andrewbazua@gmail.com>
2020-11-29 15:45:21 -0800

    feat: complete counterTick transition switch block

--- source/main.c
+++ source/main.c
@@ -1,11 +1,16 @@
-/*	Author: lab
+/*	Author: Andrew Bazua [abazu001]
  *  Partner(s) Name:
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
- *	Exercise Description: [optional - include for your own benefit]
+ *	Lab Section: 024
+ *	Assignment: Lab #11  Exercise #1
+ *	Exercise Description: [Design a system where a ‘char’ variable can be
+    incremented or decremented based on specific button presses. The value of
+    the variable is then transmitted to the shift register and displayed on a
+    bank of eight LEDs.]
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
+ *
+ *  Demo Link:
  */
 #include <avr/io.h>
 #ifdef _SIMULATE_
@@ -15,12 +20,90 @@
 #include "transmit_shift_data.h"
 #include "timer.h"
 
+typedef enum counter_states {init, wait, increment, decrement, reset,
+                             buttonPress} counter;
+
+int counterTick(int state);
+
 int main(void) {
-    /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0xFF;
 
-    /* Insert your solution below */
+    static task1;
+    task *tasks[] = {&task1};
+    const unsigned short numTasks = sizeof(tasks) / sizeof(task*);
+
+    const char start = -1;
+
+    // Task 1 (counterTick)
+    task1.state = start;
+    task1.period = 50;
+    task1.elapsedTime = task1.period;
+    task1.TickFct = &counterTick;
+    for (unsigned char i = 0; i < numTasks; i++) {
+        GCD = findGCD(GCD, tasks[i]->period);
+    }
+
+    TimerSet(GCD);
+    TimerOn();
+
     while (1) {
+        for (unsigned char i = 0; i < numTasks; i++) {
+            if (tasks[i]->elapsedTime == tasks[i]->period) {
+                tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
+                tasks[i]->elapsedTime = 0;
+            }
+            tasks[i]->elapsedTime += GCD;
+        }
 
+        while(!TimerFlag);
+        TimerFlag = 0;
     }
     return 1;
+}
+
+int counterTick(int state) {
+    static unsigned char shiftOutput;
+    unsigned char A0 = ~PINA & 0x01;
+    unsigned char A1 = ~PINA & 0x02;
+
+    switch (state) {
+        case init: state = wait; break;
+
+        case wait:
+            if (A0 && A1) { state = reset; }
+            else if (A0 && !A1) { state = increment; }
+            else if (!A0 && A1) { state = decrement; }
+            else { state = wait; }
+            break;
+
+        case increment:
+            if (A0 && A1) { state = reset; }
+            else if (A0 && !A1) { state = buttonPress; }
+            else if (!A0 && A1) { state = decrement; }
+            else { state = wait; }
+            break;
+
+        case decrement:
+            if (A0 && A1) { state = reset; }
+            else if (!A0 && A1) { state = buttonPress; }
+            else if (A0 && !A1) { state = increment; }
+            else { state = wait; }
+            break;
+
+        case reset:
+            if (A0 && A1) { state = buttonPress; }
+            else { state = wait; }
+            break;
+
+        case buttonPress:
+            if (!A0 && !A1) { state = wait; }
+            else { state = buttonPress; }
+            break;
+    }
+
+    switch (state) {
+
+    }
+
+    return state;
 }
